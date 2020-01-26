a709109a7b8ff4cfddfd472e78bda91457929e9b
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-01-26 17:05:02 +0100

    Feat: Implemented logic for the App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { CardList } from './components/card-list/card-list.component'
+import { SearchInput } from './components/searchfield/search-input.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+      isFetching: true
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      let users = await fetch('https://jsonplaceholder.typicode.com/users')
+      let usersArray = await users.json()
+      console.log('[user] ->', usersArray)
+      this.setState({
+        monsters: usersArray,
+        isFetching: false
+      })
+    } catch (e) {
+      this.setState({
+        isFetching: false,
+        monsters: [
+          { id: 1, name: 'dogg' },
+          { id: 2, name: 'sanf' },
+          { id: 3, name: 'flasq' },
+          { id: 4, name: 'klean' }
+        ]
+      })
+    }
+  }
+
+  render() {
+    const { monsters, searchField } = this.state
+    const filteredMonster = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
+    return (
+      <div className="App">
+        <h1>Monster Seeker</h1>
+        <SearchInput placeholder="Search for your monster choice" handleChange={e => this.setState({ searchField: e.target.value })} />
+        {this.state.isFetching ? <h4>Fetching monsters...</h4> : <CardList monsters={filteredMonster} />}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
